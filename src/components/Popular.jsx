2608f71489869f1d9b3aa96bddd87e2c55034b65
Pragya Gupta <pragya101997@gmail.com>
2023-10-13 22:48:03 +0530

    Adding styled components in Popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -3,17 +3,6 @@ import styled from "styled-components";
 import {Splide, SplideSlide} from '@splidejs/react-splide';
 import "@splidejs/splide/dist/css/splide.min.css";
 
-const Wrapper = styled.div`
-  margin: 4rem 0rem;
-`;
-
-const Card = styled.div`
-  min-height: 25rem;
-  border-radius: 2rem;
-  overflow: hidden;
-  img{border-radius: 2rem;}
-`;
-
 function Popular() 
 {
 
@@ -22,13 +11,24 @@ function Popular()
   useEffect(() => {
     getPopular();
   },[]);
-  
+
   const getPopular = async () => {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-    const data = await api.json();
-    setPopular(data.recipes);
-    console.log(data.recipes);
-  };
+    const check = localStorage.getItem('popular');
+
+    if(check)
+    {
+      setPopular(JSON.parse(check));
+    }
+    else
+    {
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+      const data = await api.json();
+
+      localStorage.setItem('popular', JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+      console.log(data.recipes);
+    }
+  }
 
   return (
     <div>
@@ -41,6 +41,7 @@ function Popular()
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
                 </Card>
               </SplideSlide>
             );
@@ -52,5 +53,49 @@ function Popular()
 
 }
 
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
 
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+  img 
+  { 
+    border-radius: 2rem; 
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p
+  {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%; 
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
 export default Popular;
